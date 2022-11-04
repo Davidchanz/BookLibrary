e6c4ccb8fa95d9e55a034623be8d7bd17fcfe2d2
TovlisKatsi <tovliskatsi@gmail.com>
2022-11-04 17:22:00 +0400

    init

---
+++ src/main/java/org/dbtest/Message.java
@@ -0,0 +1,61 @@
+package org.dbtest;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "message_2")
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String text;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message = (Message) o;
+        return this.id.equals(message.id) && this.text.equals(message.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, text);
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                '}';
+    }
+    public Message(){
+
+    }
+    public Message(String text){
+        this();
+        this.setText(text);
+    }
+}
